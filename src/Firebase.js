592a255f4d590cb4fd17d7fc6e1788680fb63588
Rowland <rowlandugiangbe@gmail.com>
2022-11-22 11:20:00 +0100

    netflix

---
+++ src/Firebase.js
@@ -0,0 +1,40 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP-FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER,
+//   appId: process.env.REACT_APP_APP_ID
+// };
+
+
+
+// delete later
+const firebaseConfig = {
+  apiKey: "AIzaSyBlCEnezxV8i7RyJGzhQIcZejgyxI2pOBQ",
+  authDomain: "netflix-react-8131c.firebaseapp.com",
+  projectId: "netflix-react-8131c",
+  storageBucket: "netflix-react-8131c.appspot.com",
+  messagingSenderId: "130167987553",
+  appId: "1:130167987553:web:3db35d1879919a910ff920"
+};
+
+// delete later
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app)
+// to create backend with firebase - step 1
+
+
---
+++ src/Request.js
@@ -0,0 +1,12 @@
+const key = '8306fd7f9ba7cc9840149da53745a688'
+// Go to https://www.themoviedb.org/settings/api to generate your api key
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
+
+  export default requests
